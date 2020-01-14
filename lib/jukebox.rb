cd471a1cb50a31fb8ca7e056d82cf2897a7d68fa
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-14 05:13:48 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,7 +21,7 @@ def help
 end
 
 def play(songs)
-  #puts "Please enter a song name or number:"
+  puts "Please enter a song name or number:"
   input = gets.chomp
     #binding.pry
   if songs.include?(input)
